4e96cea625ea85dcd0bb8aca1e9414cc9a5a8333
Guilherme Ferrari <guile.ferrari@hotmail.com>
2024-01-08 21:30:40 -0300

    feat: added new entity product

---
+++ src/NerdStore.Catalogo.Domain/Categoria.cs
@@ -0,0 +1,21 @@
+﻿using NerdStore.Core.DomainObjects;
+
+namespace NerdStore.Catalogo.Domain
+{
+    public class Categoria : Entity
+    {
+        public string Nome { get; set; }
+        public int Codigo { get; set; }
+
+        public Categoria(string nome, int codigo)
+        {
+            Nome = nome;
+            Codigo = codigo;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome} - {Codigo}";
+        }
+    }
+}

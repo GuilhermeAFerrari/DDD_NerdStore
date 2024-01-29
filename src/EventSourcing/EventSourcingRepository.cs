b407e23025cd99c4ef924434ae1b1d1eb78bcb43
Guilherme Ferrari <guile.ferrari@hotmail.com>
2024-01-29 20:21:19 -0300

    feat: added interfaces to event sourcing

---
+++ src/EventSourcing/EventSourcingRepository.cs
@@ -0,0 +1,25 @@
+﻿using NerdStore.Core.Data.EventSourcing;
+using NerdStore.Core.Messages;
+
+namespace EventSourcing
+{
+    public class EventSourcingRepository : IEventSourcingRepository
+    {
+        private readonly IEventStoreService _eventStoreService;
+
+        public EventSourcingRepository(IEventStoreService eventStoreService)
+        {
+            _eventStoreService = eventStoreService;
+        }
+
+        public async Task<IEnumerable<StoredEvent>> ObterEventos(Guid aggregateId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task SalvarEvento<TEvent>(TEvent evento) where TEvent : Event
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/NerdStore.Core/Data/EventSourcing/StoredEvent.cs
@@ -0,0 +1,20 @@
+﻿namespace NerdStore.Core.Data.EventSourcing
+{
+    public class StoredEvent
+    {
+        public Guid Id { get; private set; }
+        public string Tipo { get; private set; }
+        public DateTime DataOcorrencia { get; private set; }
+
+        // Dados é a serialização Json do event completo, para pode ser deserializado posteriormente
+        public string Dados { get; private set; }
+
+        public StoredEvent(Guid id, string tipo, DateTime dataOcorrencia, string dados)
+        {
+            Id = id;
+            Tipo = tipo;
+            DataOcorrencia = dataOcorrencia;
+            Dados = dados;
+        }
+    }
+}

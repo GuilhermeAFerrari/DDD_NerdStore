a2d6729bdc3c66bfaf8d732c14689d1cba470915
Guilherme Ferrari <guile.ferrari@hotmail.com>
2024-03-19 20:55:42 -0300

    tests: added tests to PedidoHandler

---
+++ tests/NerdStore.Vendas.Application.Tests/Pedidos/PedidoCommandHandlerTests.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Moq;
+using Moq.AutoMock;
+using NerdStore.Vendas.Application.Commands;
+using NerdStore.Vendas.Domain;
+
+namespace NerdStore.Vendas.Application.Tests.Pedidos
+{
+    public class PedidoCommandHandlerTests
+    {
+        [Fact(DisplayName = "Adicionar item novo pedido com sucesso")]
+        [Trait("Categoria", "Vendas - Pedido command handler")]
+        public async Task AdicionarItem_NovoPedido_DeveExecutarComSucesso()
+        {
+            // Arrange
+            var pedidoCommand = new AdicionarItemPedidoCommand(Guid.NewGuid(), Guid.NewGuid(), "Produto teste", 2, 100);
+
+            var mocker = new AutoMocker();
+            var pedidoHandler = mocker.CreateInstance<PedidoCommandHandler>();
+
+            mocker.GetMock<IPedidoRepository>()
+                .Setup(r => r.UnitOfWork.Commit())
+                .Returns(Task.FromResult(true));
+
+            // Act
+            var result = await pedidoHandler.Handle(pedidoCommand, CancellationToken.None);
+
+            // Assert
+            Assert.True(result);
+            mocker.GetMock<IPedidoRepository>().Verify(r => r.Adicionar(It.IsAny<Pedido>()), Times.Once());
+            mocker.GetMock<IPedidoRepository>().Verify(r => r.UnitOfWork.Commit(), Times.Once);
+
+            // Nesse caso de testes, não faz sentido validar o lançamento do evento, pois quem validará isso é um teste do UnitOfWork
+            //mocker.GetMock<IMediator>().Verify(x => x.Publish(It.IsAny<INotification>(), CancellationToken.None), Times.Once());
+        }
+    }
+}

bc5ed4827d93b96db1911e06951dba09dbec0726
Guilherme Ferrari <guile.ferrari@hotmail.com>
2024-03-27 21:07:07 -0300

    tests: added new integration tests

--- src/NerdStore.WebApp.MVC/Program.cs
+++ src/NerdStore.WebApp.MVC/Program.cs
@@ -8,65 +8,81 @@ using NerdStore.WebApp.MVC.Data;
 using NerdStore.WebApp.MVC.Setup;
 using System.Reflection;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddAutoMapper(
-    typeof(DomainToViewModelMappingProfile),
-    typeof(ViewModelToDomainMappingProfile)
-);
-
-builder.Services.AddMediatR(
-    c => c.RegisterServicesFromAssembly(
-        Assembly.GetExecutingAssembly()));
-
-var connectionStringDbContext = builder.Configuration.GetConnectionString("DefaultConnection") ??
-    throw new InvalidOperationException("Connection string 'CatalogoConnection' not found.");
-
-builder.Services.AddDbContext<VendasContext>(options =>
-    options.UseSqlServer(connectionStringDbContext));
-
-builder.Services.AddDbContext<CatalogoContext>(options =>
-    options.UseSqlServer(connectionStringDbContext));
-
-builder.Services.AddDbContext<PagamentoContext>(options =>
-    options.UseSqlServer(connectionStringDbContext));
-
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionStringDbContext));
-
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddControllersWithViews();
-
-builder.Services.RegisterServices();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace NerdStore.WebApp.MVC
 {
-    app.UseMigrationsEndPoint();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Configuration
+                .SetBasePath(builder.Environment.ContentRootPath)
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+                .AddEnvironmentVariables();
+
+            // Add services to the container.
+            builder.Services.AddAutoMapper(
+                typeof(DomainToViewModelMappingProfile),
+                typeof(ViewModelToDomainMappingProfile)
+            );
+
+            builder.Services.AddMediatR(
+                c => c.RegisterServicesFromAssembly(
+                    Assembly.GetExecutingAssembly()));
+
+            var connectionStringDbContext = builder.Configuration.GetConnectionString("DefaultConnection") ??
+                throw new InvalidOperationException("Connection string 'CatalogoConnection' not found.");
+
+            builder.Services.AddDbContext<VendasContext>(options =>
+                options.UseSqlServer(connectionStringDbContext));
+
+            builder.Services.AddDbContext<CatalogoContext>(options =>
+                options.UseSqlServer(connectionStringDbContext));
+
+            builder.Services.AddDbContext<PagamentoContext>(options =>
+                options.UseSqlServer(connectionStringDbContext));
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionStringDbContext));
+
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.RegisterServices();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Vitrine}/{action=Index}/{id?}");
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
 }
-else
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Vitrine}/{action=Index}/{id?}");
-app.MapRazorPages();
-
-app.Run();
